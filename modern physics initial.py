a1d68f32aabc9548f8d58d85daf3deecc9737e90
incited <85405413+incited@users.noreply.github.com>
2023-12-06 08:47:53 -0700

    Update modern physics initial.py

--- modern physics initial.py
+++ modern physics initial.py
@@ -15,6 +15,8 @@ def RHS(y):
 # np.array([3, 10])
 
 # plt.xlim() and plt.ylim() can be used for domain intervals? just a suggestion
+# idx = np.argwhere(np.diff(np.sign(f - g))).flatten()  // returns points where two graphs (f and g) cross; print(idx) returns all the x-value points of intersection
+
 
 plt.plot(np.linspace(0, 4*np.pi, 100),np.sin(np.linspace(0, 4*np.pi, 100)))
 plt.show()
