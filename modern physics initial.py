c1ceccfa9eaf15aa2d4f2016988a23968f302d19
epodol <34190213+epodol@users.noreply.github.com>
2023-12-07 13:53:10 -0700

    Remove intersection point repetition and add try/except

--- modern physics initial.py
+++ modern physics initial.py
@@ -1,81 +1,41 @@
-import numpy as np 
-from scipy.optimize import fsolve
-import matplotlib.pyplot as plt
-
-print("Enter your equation") 
-test = input()
-print("")
-def fcn(x): 
-    return (np.cos(x)-x)
-
-print(fsolve(fcn, 0))
-xpoints = np.arange(1,2, 8)
-def RHS(y):
-    return np.log(4*y)
-# np.array([3, 10])
-
-# plt.xlim() and plt.ylim() can be used for domain intervals? just a suggestion
-# idx = np.argwhere(np.diff(np.sign(f - g))).flatten()  // returns points where two graphs (f and g) cross; print(idx) returns all the x-value points of intersection
-
-
-plt.plot(np.linspace(0, 4*np.pi, 100),np.sin(np.linspace(0, 4*np.pi, 100)))
-plt.show()
-
---------------------------------------------------------------------------------------------------------------------------------------------------------
-import numpy as np
-import matplotlib.pyplot as plt
-import sympy
-x = np.arange(0, 1000)
-f = np.arange(0, 1000)
-
-g = np.cos(np.arange(0, 10, 0.01) * 2) * 1000 #why does it return value error? 
-lower = int(input("Enter a domain lower: "))
-upper = int(input("Enter a domain upper: "))
-
-plt.plot(x, f, '-')
-plt.plot(x, g, '-')
-plt.xlim(lower, upper)
-idx = np.argwhere(np.diff(np.sign(f - g))).flatten()
-plt.plot(x[idx], f[idx], 'ro')
-plt.show()
-idx = [i for i in idx if lower <= i <= upper]
-
-print(idx)
-# updates: added domain input [working] 
-# goal: remove values higher than upper domain limit, return (print) updated array; recognize user input
-# updates: removed values higher than upper domain limit (or lower than lower domain limit) 
-------------------------------------------------------------------------------------------------------------------------------------
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import fsolve
 import sympy as sp
 
-equation1_str = input("Welcome. To solve the transcendental equation, move the negative (if possible) to the other side (i.e x - sin(x)---> x = sin(x). Equation one will be the left hand side, equation two wil be the right hand side. Please enter the first equation in terms of x: ")
-equation2_str = input("Enter the second equation in terms of x: ")
+equation_str = input("Welcome. Please enter the equation in terms of x: ")
 
-x = sp.symbols('x')
-equation1 = sp.sympify(equation1_str)
-equation2 = sp.sympify(equation2_str)
+equation1 = sp.sympify(equation_str.split('=')[0])
+equation2 = sp.sympify(equation_str.split('=')[1])
 
-f = sp.lambdify(x, equation1, 'numpy')
-g = sp.lambdify(x, equation2, 'numpy')
+f = sp.lambdify(sp.symbols('x'), equation1, 'numpy')
+g = sp.lambdify(sp.symbols('x'), equation2, 'numpy')
 
 lower = float(input("Enter the lower limit of the domain: "))
 upper = float(input("Enter the upper limit of the domain: "))
 
-initial_guesses = [lower + (upper - lower) * i / 1000 for i in range(1000)]
-intersection_points = [fsolve(lambda x: f(x) - g(x), guess)[0] for guess in initial_guesses]
+# try the next few lines of code, and if there is an error, catch it
+try:
+    initial_guesses = [lower + (upper - lower) * i / 1000 for i in range(1000)]
+    intersection_points = [fsolve(lambda x: f(x) - g(x), guess)[0] for guess in initial_guesses]
 
-valid_intersection_points = [point for point in intersection_points if lower <= point <= upper]
-print("Intersection Points:", valid_intersection_points)
+    valid_intersection_points = [point for point in intersection_points if lower <= point <= upper]
 
-x_values = np.linspace(lower, upper, 1000)
+    print("Intersection Points:", set([str(round(point, 3)) for point in valid_intersection_points]))
 
-plt.plot(x_values, f(x_values), label='Equation 1')
-plt.plot(x_values, g(x_values), label='Equation 2')
 
-plt.scatter(valid_intersection_points, [f(point) for point in valid_intersection_points], color='red', zorder=5, label='Intersection Points')
-plt.legend()
-plt.show()
-#updates: recognizes user input, works for trig graphs 
+    x_values = np.linspace(lower, upper, 1000)
+
+    plt.plot(x_values, f(x_values), label='Equation 1')
+    plt.plot(x_values, g(x_values), label='Equation 2')
+
+    plt.scatter(valid_intersection_points, [f(point) for point in valid_intersection_points], color='red', zorder=5,
+                label='Intersection Points')
+    plt.legend()
+    plt.show()
+
+except:
+    print("No intersection points found in the given domain. Make sure constants (pi, e, etc.) are not used in the "
+          "equation, rather explicitly write out their values.")
+# updates: recognizes user input, works for trig graphs
 # goals: TBD; array of intersection points returns BS values; doesn't work for e**x
