297c0f0153b8e3cc428b93068925429a9309fa90
incited <85405413+incited@users.noreply.github.com>
2023-12-07 22:54:54 -0700

    Update modern physics initial.py

--- modern physics initial.py
+++ modern physics initial.py
@@ -4,19 +4,17 @@ from scipy.optimize import fsolve
 import sympy as sp
 
 equation_str = input("Welcome. Please enter the equation in terms of x: ")
-
+x = sp.symbols('x')
 equation1 = sp.sympify(equation_str.split('=')[0])
 equation2 = sp.sympify(equation_str.split('=')[1])
-
-f = sp.lambdify(sp.symbols('x'), equation1, 'numpy')
-g = sp.lambdify(sp.symbols('x'), equation2, 'numpy')
+f = sp.lambdify(x, equation1, 'numpy')
+g = sp.lambdify(x, equation2, 'numpy')
 
 lower = float(input("Enter the lower limit of the domain: "))
 upper = float(input("Enter the upper limit of the domain: "))
 
-# try the next few lines of code, and if there is an error, catch it
 try:
-    initial_guesses = [lower + (upper - lower) * i / 1000 for i in range(1000)]
+    initial_guesses = [lower + (upper - lower) * i / 50 for i in range(50)]
     intersection_points = [fsolve(lambda x: f(x) - g(x), guess)[0] for guess in initial_guesses]
 
     valid_intersection_points = [point for point in intersection_points if lower <= point <= upper]
@@ -24,7 +22,8 @@ try:
     print("Intersection Points:", set([str(round(point, 3)) for point in valid_intersection_points]))
 
 
-    x_values = np.linspace(lower, upper, 1000)
+
+    x_values = np.linspace(lower, upper)
 
     plt.plot(x_values, f(x_values), label='Equation 1')
     plt.plot(x_values, g(x_values), label='Equation 2')
@@ -37,5 +36,3 @@ try:
 except:
     print("No intersection points found in the given domain. Make sure constants (pi, e, etc.) are not used in the "
           "equation, rather explicitly write out their values.")
-# updates: recognizes user input, works for trig graphs
-# goals: TBD; array of intersection points returns BS values; doesn't work for e**x
