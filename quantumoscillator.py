4ea6593bb723c8f80225d5ab5a9ea960c0375d9b
incited <85405413+incited@users.noreply.github.com>
2023-12-20 19:34:07 -0700

    Update quantumoscillator.py

--- quantumoscillator.py
+++ quantumoscillator.py
@@ -1,5 +1,5 @@
 import sympy as sp
-print("Welcome. You will be entering values for w (measured in radians per second and n (an integer)). This will return the energy state for the quantum harmonic oscillator. \nInput your value for w in the 10^7 and 10^8 range.")
+print("Welcome. You will be entering values for w (angular frequency measured in radians per second) and n (an integer)). This will return the energy state for the quantum harmonic oscillator. \nInput your value for w in the 10^7 and 10^8 range.")
 h_bar = 1.054571817 * 10**(-34) 
 n = int(input("Enter the value for n (integer): "))
 w = float(sp.sympify(input("Enter the angular frequency: ")))
