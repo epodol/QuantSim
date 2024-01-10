8824528388297e3336cf9ec843909e1f7ba96d27
incited <85405413+incited@users.noreply.github.com>
2024-01-10 13:42:26 -0700

    Update quantumoscillator.py

--- quantumoscillator.py
+++ quantumoscillator.py
@@ -1,7 +1,14 @@
 import sympy as sp
-print("Welcome. You will be entering values for w (angular frequency measured in radians per second) and n (an integer)). This will return the energy state for the quantum harmonic oscillator. \nInput your value for w in the 10^7 and 10^8 range.")
+import numpy as np  
+from sympy import Derivative
+print("Welcome. You will be entering values for w (measured in radians per second and n (an integer)). This will return the energy state for the quantum harmonic oscillator. \nInput your value for w in the 10^7 and 10^8 range.")
 h_bar = 1.054571817 * 10**(-34) 
+x = sp.symbols('x')
 n = int(input("Enter the value for n (integer): "))
 w = float(sp.sympify(input("Enter the angular frequency: ")))
+m = float(sp.sympify("Enter the mass: "))
 energy_state = (1/2 + n) * h_bar * w
 print("Energy state is ", energy_state, " joules")
+a = m*w/h_bar
+y = np.sqrt(a) * x
+hermite = (-1**n)*(np.e**x**2) #nth derivative of e**-x**2
