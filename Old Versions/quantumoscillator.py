23e3625adbd91fad0458f92ef320b9287be653e2
incited <85405413+incited@users.noreply.github.com>
2024-01-10 14:02:31 -0700

    Update quantumoscillator.py
    updated nth derivative calculation

--- Old Versions/quantumoscillator.py
+++ Old Versions/quantumoscillator.py
@@ -1,14 +1,14 @@
 import sympy as sp
 import numpy as np  
-from sympy import Derivative
+from sympy import diff 
 print("Welcome. You will be entering values for w (measured in radians per second and n (an integer)). This will return the energy state for the quantum harmonic oscillator. \nInput your value for w in the 10^7 and 10^8 range.")
 h_bar = 1.054571817 * 10**(-34) 
 x = sp.symbols('x')
 n = int(input("Enter the value for n (integer): "))
 w = float(sp.sympify(input("Enter the angular frequency: ")))
-m = float(sp.sympify("Enter the mass: "))
+m = float(sp.sympify(input("Enter the mass: ")))
 energy_state = (1/2 + n) * h_bar * w
 print("Energy state is ", energy_state, " joules")
 a = m*w/h_bar
 y = np.sqrt(a) * x
-hermite = (-1**n)*(np.e**x**2) #nth derivative of e**-x**2
+hermite = (-1)**n*(np.e**x**2)*(diff(np.e**(-x**2), x, n))
