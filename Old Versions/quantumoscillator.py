aa4a895ae6b1049718f0795664b2e37f8db65797
incited <85405413+incited@users.noreply.github.com>
2024-01-10 18:20:08 -0700

    Update quantumoscillator.py

--- Old Versions/quantumoscillator.py
+++ Old Versions/quantumoscillator.py
@@ -2,6 +2,7 @@ import sympy as sp
 import numpy as np  
 from sympy import diff 
 import math
+import matplotlib.pyplot as plt 
 print("Welcome. You will be entering values for w (measured in radians per second and n (an integer)). This will return the energy state for the quantum harmonic oscillator. \nInput your value for w in the 10^7 and 10^8 range.")
 h_bar = 1.054571817 * 10**(-34) 
 x = sp.symbols('x')
@@ -15,3 +16,7 @@ y = np.sqrt(a) * x
 hermite = (-1)**n*(np.e**x**2)*(diff(np.e**(-x**2), x, n))
 wave_fncn = ((a/np.pi)**0.25)*(1/np.sqrt((2**n)*math.factorial(n))) * hermite * (np.e*(((-y)**2)/2))
 print(wave_fncn)
+fncn = sp.lambdify(x, wave_fncn, 'numpy')
+x_vals = np.linspace(-1000, 1000, 1000) #idk what values to put here
+plt.plot(x_vals, fncn(x_vals))
+plt.show()
