393371500cbf975a819c8a51c567422f3665442d
9Black7Hole <85371491+9Black7Hole@users.noreply.github.com>
2024-01-14 09:51:39 -0700

    InfiniteWell.py

--- Scenarios/InfiniteWell.py
+++ Scenarios/InfiniteWell.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 # Define constants
 L = float(input("Enter the width of the well: "))  # Width of the well
-N = float(input("Enter the energy level of the well: "))  # Number of energy levels to plot
+N = int(input("Enter the energy level of the well: "))  # Number of energy levels to plot
 x_values = np.linspace(0, L, 1000)  # Values of x within the well
 
 # Define the wave function for an infinite well
@@ -17,7 +17,7 @@ for n in range(1, N + 1):
 # Set plot labels and title
 plt.xlabel('Position (x)')
 plt.ylabel('Wave Function')
-plt.title('Wave Functions for an Infinite Well')
+plt.title('Wave Functions for an Infinite Square Well')
 
 # Show the plot with a legend
 plt.legend()
