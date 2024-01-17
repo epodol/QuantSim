e8f214043f63d091d6a5b74ee1711d13106971cb
incited <85405413+incited@users.noreply.github.com>
2024-01-17 11:08:24 -0700

    Update QuantumOscillator.py

--- Scenarios/QuantumOscillator.py
+++ Scenarios/QuantumOscillator.py
@@ -5,7 +5,7 @@ import math
 import matplotlib.pyplot as plt 
 from scipy.integrate import quad 
 print("Welcome. You will be entering values for w (measured in radians per second and n (an integer)). This will return the energy state, the hermite polynomial, the spacial function, graphs of the probability density function and g(x), and the probability for the quantum harmonic oscillator. \nInput your value for w in the 10^7 and 10^8 range.")
-h_bar = 1.054571817 * 10**(-34) 
+h_bar = 1.054571817 * 10**(-34) # in joule-seconds
 x = sp.symbols('x')
 y = sp.symbols('y')
 n = int(input("Enter the value for n (integer): "))
@@ -17,12 +17,12 @@ energy_state = (1/2 + n) * h_bar * w
 print("Energy state is: ", energy_state, "Joules")
 a = m*w/h_bar
 hermite = ((-1)**n)*(np.e**y**2)*(diff(np.e**(-y**2), y, n))
-print("Hermite before substitution: " , hermite)  
+print("Hermite Polynomial for n =",n ,": ", hermite)  
 hermite = hermite.subs(y, (sp.sqrt(a) * x))
 print("Hermite after substitution: ", hermite)
 wave_fncn = (((a/np.pi)**0.25)*(1/(np.sqrt((2**n)*math.factorial(n)))) * hermite * (np.e**((-(y**2))/2)))
 wave_fncn = wave_fncn.subs(y, sp.sqrt(a) * x)
-print("Spacial function g(x): ", wave_fncn)
+print("Spacial Function g(x): ", wave_fncn)
 fncn = sp.lambdify(x, wave_fncn, 'scipy')
 x_vals = np.linspace(x1, x2, 1000000) 
 def integrand(x_values) :
