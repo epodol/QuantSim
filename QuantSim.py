33e36e34a8c5b6fd8b5bfe17fb847c0036387ee3
epodol <34190213+epodol@users.noreply.github.com>
2024-01-17 13:15:59 -0700

    Incorporate InfiniteWell into main program

--- QuantSim.py
+++ QuantSim.py
@@ -2,6 +2,7 @@ import sympy as sp
 import warnings
 
 from Scenarios.FiniteWell import finite_well
+from Scenarios.InfiniteWell import infinite_well
 from Scenarios.QuantumOscillator import quantum_oscillator
 
 # Suppress RuntimeWarnings for prod
@@ -17,7 +18,7 @@ def main():
             finite_well()
         case 2:
             print("Infinite Well Scenario")
-            # TODO: Implement Infinite Well
+            infinite_well()
         case 3:
             print("Quantum Oscillator Scenario")
             quantum_oscillator()
--- Scenarios/InfiniteWell.py
+++ Scenarios/InfiniteWell.py
@@ -1,21 +1,20 @@
 import sympy as sp
 import numpy as np  
 import matplotlib.pyplot as plt 
-# Define constants
-L = float(sp.sympify((input("Enter the width of the well: "))))  # Width of the well
-N = int(input("Enter the energy level of the well: "))  # Number of energy levels to plot
-x_values = np.linspace(0, L, 10000)  # Values of x within the well
 
-# Define the wave function for an infinite well
-def psi_n(n, x):
-    return np.sqrt(2 / L) * np.sin(n * np.pi * x / L)
 
-for n in range(1, N + 1):
-    plt.plot(x_values, psi_n(n, x_values), label=f'n={n}')
+def infinite_well():
+    width = float(sp.sympify((input("Enter the width of the well: "))))  # Width of the well
+    energy_levels = int(input("Enter the energy level of the well: "))  # Number of energy levels to plot
+    x_values = np.linspace(0, width, 10000)  # Values of x within the well
 
-plt.xlabel('Position (x)')
-plt.ylabel('Wave Function')
-plt.title('Wave Functions for an Infinite Square Well')
+    # Define the wave function for an infinite well
+    for n in range(1, energy_levels + 1):
+        plt.plot(x_values, np.sqrt(2 / width) * np.sin(n * np.pi * x_values / width), label=f'n={n}')
 
-plt.legend()
-plt.show()
+    plt.xlabel('Position (x)')
+    plt.ylabel('Wave Function')
+    plt.title('Wave Functions for an Infinite Square Well')
+
+    plt.legend()
+    plt.show()
