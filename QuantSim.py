aea0a442dc0325523c06d049982f8639bfe70857
epodol <34190213+epodol@users.noreply.github.com>
2024-01-17 13:04:22 -0700

    Update QuantSim

--- QuantSim.py
+++ QuantSim.py
@@ -7,22 +7,23 @@ from Scenarios.QuantumOscillator import quantum_oscillator
 # Suppress RuntimeWarnings for prod
 warnings.filterwarnings('ignore', category=RuntimeWarning)
 
-x = sp.symbols('x')
-choice = int(input("Please choose a scenario number:\nFinite Well (E < U_o) [1]\nInfinite Well [2]\nHarmonic "
-                   "Oscillator [3] Quantum Oscillator\n"))
-print("Next, you will enter parameters. A couple of tips while choosing these parameters:\n"
-      "1. Potential energy should be in the eV range (1 eV = 1.602*10^(-19) Joules).\n"
-      "2. Mass should be on a quantum scale (similar to the mass of an electron).")
 
-match choice:
-    case 1:
-        finite_well()
-    case 2:
-        print("Infinite Well Scenario")
-        # TODO: Implement Infinite Well
-    case 3:
-        print("Quantum Oscillator Scenario")
-        quantum_oscillator()
-    case _:
-        print("Please enter a valid scenario input (1, 2, or 3).")
-        quit()
+def main():
+    choice = int(input("Please choose a scenario number:\nFinite Well (E < U_o) [1]\nInfinite Well [2]\nHarmonic "
+                       "Oscillator [3] Quantum Oscillator\n"))
+
+    match choice:
+        case 1:
+            finite_well()
+        case 2:
+            print("Infinite Well Scenario")
+            # TODO: Implement Infinite Well
+        case 3:
+            print("Quantum Oscillator Scenario")
+            quantum_oscillator()
+        case _:
+            print("Please enter a valid scenario input (1, 2, or 3).")
+            main()
+
+
+main()
